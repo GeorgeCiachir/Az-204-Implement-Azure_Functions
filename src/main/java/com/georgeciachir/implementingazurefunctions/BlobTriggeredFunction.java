6d406acd070138ed4c2dc3bebb4950fd52f6af3a
GeorgeCiachir <ciachirg@ebrd.com>
2022-03-06 14:09:52 +0200

    Az-204 add blob triggered function

---
+++ src/main/java/com/georgeciachir/implementingazurefunctions/BlobTriggeredFunction.java
@@ -0,0 +1,24 @@
+package com.georgeciachir.implementingazurefunctions;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.annotation.BindingName;
+import com.microsoft.azure.functions.annotation.BlobTrigger;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.StorageAccount;
+
+/**
+ * Azure Functions with Azure Blob trigger.
+ */
+public class BlobTriggeredFunction {
+    /**
+     * This function will be invoked when a new or updated blob is detected at the specified path. The blob contents are provided as input to this function.
+     */
+    @FunctionName("blob-triggered-function")
+    @StorageAccount("AzureWebJobsStorage")
+    public void run(
+            @BlobTrigger(name = "content", path = "some-blob-location/{name}", dataType = "binary") byte[] content,
+            @BindingName("name") String name,
+            final ExecutionContext context) {
+        context.getLogger().info("Java Blob triggered function processed a blob. Name: " + name + "\n  Size: " + content.length + " Bytes");
+    }
+}
