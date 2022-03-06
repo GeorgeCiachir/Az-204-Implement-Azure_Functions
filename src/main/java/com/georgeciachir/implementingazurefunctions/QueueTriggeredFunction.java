6c79847e405cb92a9a221e568a56a2adaeda7147
GeorgeCiachir <ciachirg@ebrd.com>
2022-03-06 14:36:55 +0200

    Az-204 add queue triggered function

---
+++ src/main/java/com/georgeciachir/implementingazurefunctions/QueueTriggeredFunction.java
@@ -0,0 +1,20 @@
+package com.georgeciachir.implementingazurefunctions;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.QueueTrigger;
+
+/**
+ * Azure Functions with Azure Storage Queue trigger.
+ */
+public class QueueTriggeredFunction {
+    /**
+     * This function will be invoked when a new message is received at the specified path. The message contents are provided as input to this function.
+     */
+    @FunctionName("QueueTriggeredFunction")
+    public void queueTrigger(
+            @QueueTrigger(name = "message", queueName = "queue-items", connection = "AzureWebJobsStorage") String message,
+            final ExecutionContext context) {
+        context.getLogger().info("Java Queue trigger function processed a message: " + message);
+    }
+}
